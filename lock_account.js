b947594c5fe8c086f99ebb506ccfa8589c98e72d
Shane Tomlinson <stomlinson@mozilla.com>
2015-03-11 17:35:52 +0000

    Initial commit.

---
+++ lock_account.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var program = require('commodore');
+var FxaClient = require('fxa-js-client');
+
+
+program
+  .version('0.0.1')
+  .option('-a, --auth <auth_server>', 'auth server to use. default: https://stomlinson.dev.lcip.org/auth/v1')
+  .option('-e, --email <email>', 'email of account to lock')
+  .demand('e')
+  .option('-p, --pw <password>', 'password of account to lock')
+  .demand('pw')
+  .parse(process.argv);
+
+
+var email = program.email;
+var password = program.pw;
+var authServer = program.auth || 'https://stomlinson.dev.lcip.org/auth/v1';
+
+console.log('(%s) locking %s with %s', authServer, email, password);
+
+var fxaClient = new FxaClient(authServer);
+fxaClient.accountLock(email, password)
+  .then(function () {
+    console.log('%s locked', email);
+    process.exit(0);
+  }, function (err) {
+    console.error(String(err.message));
+    process.exit(err.errno);
+  });
